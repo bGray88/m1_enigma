4351d4269226e5a4dbca5f7201dc7a813eec68f7
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-13 12:05:52 -0700

    test: preserve special characters

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -11,6 +11,10 @@ class Encrypt
     @spec_chars_set = ('!'..'?').to_a.concat(['_', '\\', '@'])
   end
 
+  def process(message, key = nil, date = nil)
+    spec_chars = preserve_special_characters(message)
+  end
+
   def process_date(date)
     date = Time.now.strftime('%d%m%y') if date.nil?
     offset = date.to_i ** 2
--- spec/encrypt_spec.rb
+++ spec/encrypt_spec.rb
@@ -25,4 +25,13 @@ describe Encrypt do
       expect(encrypt.process_date(nil)).to eq([5, 5, 5, 5])
     end
   end
+
+  describe '#preserve_special_characters' do
+    it 'reserves special characters and their placement in the message for restoration later' do
+      encrypt = Encrypt.new
+
+      expect(encrypt.preserve_special_characters('Hello!')).to eq([[5, "!"]])
+      expect(encrypt.preserve_special_characters('')).to eq([])
+    end
+  end
 end
