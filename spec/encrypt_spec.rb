2b6a195ff751761a0dcc7fbbff5e3be403747ec2
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-13 14:46:13 -0700

    test: reverse shift for decrypt

--- spec/encrypt_spec.rb
+++ spec/encrypt_spec.rb
@@ -110,4 +110,17 @@ describe Encrypt do
       expect(encrypt.encrypt('hello world', all_shifts).join).to eq(expected)
     end
   end
+
+  describe 'reverse_shifts' do
+    it 'converts the integer elements of full collection of' do
+      encrypt        = Encrypt.new
+      converted_key  = Key.new('02715').process_key
+      converted_date = encrypt.process_date(date)
+      single_shifts  = encrypt.create_shifts(converted_key, converted_date)
+      all_shifts     = encrypt.assemble_shifts_full(message, single_shifts)
+      expected       = [-3, -27, -73, -20, -3, -27, -73, -20, -3, -27, -73, -20]
+
+      expect(encrypt.reverse_shifts(all_shifts)).to eq(expected)
+    end
+  end
 end
