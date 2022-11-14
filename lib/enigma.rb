6be6786a71d672875ffdef13990ceaa02ae80873
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-13 18:08:50 -0700

    feat: adds decrypt to enigma instance to enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 
 require_relative '../lib/encrypter'
+require_relative '../lib/decrypter'
 
 # Enigma class
 class Enigma
   def initialize
     @encrypter = Encrypter.new
+    @decrypter = Decrypter.new
   end
 
   def encrypt(message, key = nil, date = nil)
     @encrypter.process(message, key, date)
+  end
+
+  def decrypt(message, key = nil, date = nil)
+    @decrypter.process(message, key, date)
   end
 end
--- runner.rb
+++ runner.rb
@@ -7,3 +7,13 @@ time = Benchmark.measure {
   }
 }
 puts time.real
+
+enigma = Enigma.new
+p encrypted = enigma.encrypt("hello world!?", "02715", "40895")
+p decrypted = enigma.decrypt(encrypted[:encryption], encrypted[:key], encrypted[:date])
+p encrypted = enigma.encrypt("Super_Fan", "52648")
+p decrypted = enigma.decrypt(encrypted[:encryption], encrypted[:key], encrypted[:date])
+p encrypted = enigma.encrypt("Je$us Fr3Ak!", "33333")
+p decrypted = enigma.decrypt(encrypted[:encryption], encrypted[:key], encrypted[:date])
+p encrypted = enigma.encrypt("cr3@M\"dC0rN")
+p decrypted = enigma.decrypt(encrypted[:encryption], encrypted[:key])
