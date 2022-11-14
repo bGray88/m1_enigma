c6596f6b7079f8a930aea581e6c7ccd4ccf7dac8
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-13 18:36:12 -0700

    small miscellaneous changes to runner and encrypter

--- lib/encrypter.rb
+++ lib/encrypter.rb
@@ -10,6 +10,7 @@ class Encrypter < Crypt
     crypt_data = super
     shifts_update     = assemble_shifts_full(crypt_data[:converted_message], crypt_data[:shifts])
     encrypted_message = encrypt_decrypt(crypt_data[:converted_message], shifts_update)
-    { encryption: restore_spec_chars(encrypted_message, crypt_data[:spec_chars]), key: crypt_data[:converted_key].key, date: format_date(date).to_s }
+    { encryption: restore_spec_chars(encrypted_message, crypt_data[:spec_chars]),
+      key: crypt_data[:converted_key].key, date: format_date(date).to_s }
   end
 end
