a297d07a8422a53797bdb1fffc55c9604977c2b2
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-14 08:54:17 -0700

    test: fix format date test

--- spec/timeable_spec.rb
+++ spec/timeable_spec.rb
@@ -27,11 +27,10 @@ describe Timeable do
 
   describe '#format_date' do
     it 'returns a simplified date time string and a simplified version of todays date if arg is nil' do
-      time = double('time')
-      allow(time).to receive(:format_time_to_simple).and_return(131122)
+      allow(dummy).to receive(:randomize_date).and_return('131122')
       
       expect(dummy.format_date(Time.parse('August 4, 1995'))).to eq('040895')
-      expect(dummy.format_date(nil)).to eq(131122)
+      expect(dummy.format_date(nil)).to eq('131122')
     end
   end
 end
